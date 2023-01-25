44b97ed356861e7bb2660e528c32206708feba27
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-01-25 15:29:08 +0200

    d

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -42,32 +42,32 @@ function onFormSubmit(evt) {
 function onEmailAndMessageInput(evt) {
   formData[evt.target.name] = evt.target.value;
 
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  //   save(STORAGE_KEY, formData);
+  // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    save(STORAGE_KEY, formData);
 }
 
 function populateTextarea() {
-  //     load(STORAGE_KEY);
+      load(STORAGE_KEY);
 
-  //   refs.input.value = serializedState.email;
-  //   refs.textarea.value = serializedState.message;
+    refs.input.value = serializedState.email;
+    refs.textarea.value = serializedState.message;
 
-  //   formData.email = serializedState.email;
-  //   formData.message = serializedState.message;
+    formData.email = serializedState.email;
+    formData.message = serializedState.message;
 
-  //   return serializedState;
+    return serializedState;
 
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  // const savedMessage = localStorage.getItem(STORAGE_KEY);
 
-  if (savedMessage) {
-    const savedEmailAndPassword = JSON.parse(savedMessage);
+  // if (savedMessage) {
+  //   const savedEmailAndPassword = JSON.parse(savedMessage);
 
-    refs.input.value = savedEmailAndPassword.email;
-    refs.textarea.value = savedEmailAndPassword.message;
+  //   refs.input.value = savedEmailAndPassword.email;
+  //   refs.textarea.value = savedEmailAndPassword.message;
 
-    formData.email = savedEmailAndPassword.email;
-    formData.message = savedEmailAndPassword.message;
+  //   formData.email = savedEmailAndPassword.email;
+  //   formData.message = savedEmailAndPassword.message;
 
-    return savedEmailAndPassword;
-  }
+  //   return savedEmailAndPassword;
+  // }
 }
